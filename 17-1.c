c2c1438edd6c195f8f8ee3b90356bff3561ddb8f
Denis Denisov <d.l.denisov@gmail.com>
2021-12-15 11:54:20 +0300

    F to C: add formatted output

--- 17-1.c
+++ 17-1.c
@@ -13,7 +13,7 @@ int main(int argc, char const *argv[])
     while (fahr <= upper)
     {
         celsius = 5 * (fahr - 32) / 9;
-        printf("%d\t%d\n", fahr, celsius);
+        printf("%3d %6d\n", fahr, celsius);
         fahr = fahr + step;
     }
     
