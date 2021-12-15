a6205bd72db8cc087c4977bf7a57534bf252b9f6
Denis Denisov <d.l.denisov@gmail.com>
2021-12-15 17:51:15 +0300

    F to C: using constants

---
+++ 22-1.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#define LOWER   0
+#define UPPER   300
+#define STEP    20
+
+int main(int argc, char const *argv[])
+{
+    float fahr;
+
+    printf("  F  |    C\n");
+    printf("--------------\n");
+    for(fahr = LOWER; fahr <= UPPER; fahr = fahr + STEP)
+        printf(" %3.0f | %6.1f\n", fahr, (5.0/9.0) * (fahr - 32));
+    printf("--------------\n");
+    return 0;
+}
