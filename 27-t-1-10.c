ec7d0e4b3b8a5ec51ea1853ed3785a07146277fe
Denis Denisov <d.l.denisov@gmail.com>
2021-12-19 02:13:58 +0300

    task 1.10: not sure if it it correct...

---
+++ 27-t-1-10.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(int argc, char const *argv[])
+{
+    int c;
+
+    while ( (c = getchar()) != EOF )
+    {
+        if (c == '\t')
+            printf("\\t");
+        if (c == '\b')
+            printf("\\b");
+        if (c == '\\')
+            printf("\\");
+    }
+    return 0;
+}
